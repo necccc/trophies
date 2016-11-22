b3e7d69434e950e55f3448ec58cc4d4ff580cdd0
_nec <nec@shell8.net>
2016-11-22 22:20:03 +0100

    book parsing

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 const electron = require('electron')
+const contextMenu = require('./lib/contextmenu')
+const safaribooks = require('./lib/safaribooks') 
 
 const { app, Menu, BrowserWindow } = electron;
 
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
-let ctxMenuTemplate = [
-    {
-      role: 'quit'
-    }
-]
 
 function createWindow () {
   mainWindow = new BrowserWindow({
@@ -29,46 +23,14 @@ function createWindow () {
 
   let contents = mainWindow.webContents
 
+  const book = safaribooks(contents) 
+
   contents.openDevTools()
 
   contents.on('did-finish-load', () => {
-
   })
 
-  contents.on('context-menu', (event, params)=> {
-    console.log(event, params)
-
-
-    let ctxTpl = [...ctxMenuTemplate]
-    let ctxMenu = {}
-
-    ctxTpl.unshift({
-      label: 'Go back to Safari Books Online',
-      click () {
-        ctxMenu = null
-        ctxTpl = null
-        mainWindow.loadURL('https://www.safaribooksonline.com/')
-      }
-    })
-
-    ctxTpl.unshift({
-      label: 'Save to EPUB',
-      enabled: false,
-      click () {
-
-// van e konyv epp megnyitva - ha nincs, akkor disabled
-// mi a cime
-// Save 'BOOK TITLE' as [EPUB]
-
-        console.log('clicked Save to EPUB')
-        ctxMenu = null
-        ctxTpl = null
-      }
-    })
-    ctxMenu = Menu.buildFromTemplate(ctxTpl)
-
-    ctxMenu.popup(mainWindow, params.x, params.y)
-  })
+  contents.on('context-menu', (event, params) => contextMenu(book, mainWindow, params.x, params.y))
 
   mainWindow.on('closed', function () {
     mainWindow = null
@@ -76,17 +38,17 @@ function createWindow () {
   })
 }
 
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
-
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
 app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
